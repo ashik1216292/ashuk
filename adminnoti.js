172fc9465027b819221034353504f62d611a2f54
Ashik <iloveyoucheak123@gmail.com>
2025-08-11 19:14:10 +0600

    Add files via upload

--- adminnoti.js
+++ adminnoti.js
@@ -1,126 +1,115 @@
+const fs = require('fs');
+const request = require('request');
+
 module.exports.config = {
-  name: "adminNoti",
-  eventType: [
-    "log:thread-admins",
-    "log:user-nickname",
-    "log:thread-call",
-    "log:thread-icon",
-    "log:thread-color",
-    "log:link-status",
-    "log:magic-words",
-    "log:thread-approval-mode",
-    "log:thread-poll"
-  ],
-  version: "1.0.1",
-  credits: "Mirai Team & mod by Yan Maglinte",
-  description: "Group Information Update",
-  envConfig: {
-    autoUnsend: true,
-    sendNoti: true,
-    timeToUnsend: 10
-  }
-};
+    name: "adminnoti",
+    version: "1.0.0",
+    permission: 2,
+    credits: "Nayan",
+    description: "",
+    prefix: true,
+    category: "admin",
+    usages: "[msg]",
+    cooldowns: 5,
+}
 
-module.exports.run = async function({ event, api, Threads, Users }) {
-  const { author, threadID, logMessageType, logMessageData, logMessageBody } = event;
-  const { setData, getData } = Threads;
-  const fs = require("fs");
-  const iconPath = __dirname + "/cache/emoji.json";
-  if (!fs.existsSync(iconPath)) fs.writeFileSync(iconPath, JSON.stringify({}));
-  if (author === threadID) return;
+let atmDir = [];
 
-  try {
-    let dataThread = (await getData(threadID)).threadInfo;
-
-    switch (logMessageType) {
-      case "log:thread-admins": {
-        if (logMessageData.ADMIN_EVENT === "add_admin") {
-          dataThread.adminIDs.push({ id: logMessageData.TARGET_ID });
-          api.sendMessage(`[ GROUP UPDATE ]\n❯ USER UPDATE ${Users.getNameUser(logMessageData.TARGET_ID)} Became a group admin`, threadID);
-        } else if (logMessageData.ADMIN_EVENT === "remove_admin") {
-          dataThread.adminIDs = dataThread.adminIDs.filter(item => item.id !== logMessageData.TARGET_ID);
-          api.sendMessage(`[ GROUP UPDATE ]\n❯ Remove user's admin position ${logMessageData.TARGET_ID}`, threadID);
-        }
-        break;
-      }
-      case "log:user-nickname": {
-        const { participant_id, nickname } = logMessageData;
-        if (participant_id && nickname) {
-          dataThread.nicknames = dataThread.nicknames || {};
-          dataThread.nicknames[participant_id] = nickname;
-          const participantName = await Users.getNameUser(participant_id);
-          const formattedNickname = nickname || "deleted nickname";
-          api.sendMessage(`[ GROUP ]\n❯ Updated nickname for ${participantName}: ${formattedNickname}.`, threadID);
-        }
-        break;
-      }
-      case "log:thread-icon": {
-        const preIcon = JSON.parse(fs.readFileSync(iconPath));
-        dataThread.threadIcon = logMessageData.thread_icon || "👍";
-        if (global.configModule[this.config.name].sendNoti) {
-          api.sendMessage(`[ GROUP UPDATE ]\n❯ ${logMessageBody.replace("emoji", "icon")}\n❯ Original Emoji: ${preIcon[threadID] || "unknown"}`, threadID, async (error, info) => {
-            preIcon[threadID] = dataThread.threadIcon;
-            fs.writeFileSync(iconPath, JSON.stringify(preIcon));
-            if (global.configModule[this.config.name].autoUnsend) {
-              await new Promise(resolve => setTimeout(resolve, global.configModule[this.config.name].timeToUnsend * 1000));
-              return api.unsendMessage(info.messageID);
-            }
-          });
-        }
-        break;
-      }
-      case "log:thread-call": {
-        if (logMessageData.event === "group_call_started") {
-          const name = await Users.getNameUser(logMessageData.caller_id);
-          api.sendMessage(`[ GROUP UPDATE ]\n❯ ${name} STARTED A ${(logMessageData.video) ? 'VIDEO ' : ''}CALL.`, threadID);
-        } else if (logMessageData.event === "group_call_ended") {
-          const callDuration = logMessageData.call_duration;
-          const hours = Math.floor(callDuration / 3600);
-          const minutes = Math.floor((callDuration - (hours * 3600)) / 60);
-          const seconds = callDuration - (hours * 3600) - (minutes * 60);
-          const timeFormat = `${hours}:${minutes}:${seconds}`;
-          api.sendMessage(`[ GROUP UPDATE ]\n❯ ${(logMessageData.video) ? 'Video' : ''} call has ended.\n❯ Call duration: ${timeFormat}`, threadID);
-        } else if (logMessageData.joining_user) {
-          const name = await Users.getNameUser(logMessageData.joining_user);
-          api.sendMessage(`❯ [ GROUP UPDATE ]\n❯ ${name} Joined the ${(logMessageData.group_call_type == '1') ? 'Video' : ''} call.`, threadID);
-        }
-        break;
-      }
-      case "log:link-status": {
-        api.sendMessage(logMessageBody, threadID);
-        break;
-      }
-      case "log:magic-words": {
-        api.sendMessage(`» [ GROUP UPDATE ] Theme ${logMessageData.magic_word} added effect: ${logMessageData.theme_name}\nEmoij: ${logMessageData.emoji_effect || "No emoji "}\nTotal ${logMessageData.new_magic_word_count} word effect added`, threadID)
-        break;
-      }
-      case "log:thread-poll": {
-        const obj = JSON.parse(logMessageData.question_json);
-        if (logMessageData.event_type === "question_creation" || logMessageData.event_type === "update_vote") {
-          api.sendMessage(logMessageBody, threadID);
-        }
-        break;
-      }
-      case "log:thread-approval-mode": {
-        api.sendMessage(logMessageBody, threadID);
-        break;
-      }
-      case "log:thread-color": {
-        dataThread.threadColor = logMessageData.thread_color || "🌤";
-        if (global.configModule[this.config.name].sendNoti) {
-          api.sendMessage(`[ GROUP UPDATE ]\n❯ ${logMessageBody.replace("Theme", "color")}`, threadID, async (error, info) => {
-            if (global.configModule[this.config.name].autoUnsend) {
-              await new Promise(resolve => setTimeout(resolve, global.configModule[this.config.name].timeToUnsend * 1000));
-              return api.unsendMessage(info.messageID);
-            }
-          });
-        }
-        break;
-      }
+const getAtm = (atm, body) => new Promise(async (resolve) => {
+    let msg = {}, attachment = [];
+    msg.body = body;
+    for(let eachAtm of atm) {
+        await new Promise(async (resolve) => {
+            try {
+                let response =  await request.get(eachAtm.url),
+                    pathName = response.uri.pathname,
+                    ext = pathName.substring(pathName.lastIndexOf(".") + 1),
+                    path = __dirname + `/cache/${eachAtm.filename}.${ext}`
+                response
+                    .pipe(fs.createWriteStream(path))
+                    .on("close", () => {
+                        attachment.push(fs.createReadStream(path));
+                        atmDir.push(path);
+                        resolve();
+                    })
+            } catch(e) { console.log(e); }
+        })
     }
+    msg.attachment = attachment;
+    resolve(msg);
+})
 
-    await setData(threadID, { threadInfo: dataThread });
-  } catch (error) {
-    console.log(error);
-  }
-};
+module.exports.handleReply = async function ({ api, event, handleReply, Users, Threads, getText }) {
+    
+    const moment = require("moment-timezone");
+      var gio = moment.tz("Asia/Manila").format("DD/MM/YYYY - HH:mm:s");
+    const { threadID, messageID, senderID, body } = event;
+    let name = await Users.getNameUser(senderID);
+    switch (handleReply.type) {
+        case "sendnoti": {
+            let text = `${name} replied to your announce\n\ntime : ${gio}\nreply : ${body}\n\nfrom group : ${(await Threads.getInfo(threadID)).threadName || "unknown"}`;
+            if(event.attachments.length > 0) text = await getAtm(event.attachments, `${body}${name} replied to your announce\n\ntime : ${gio}\n\nfrom group : ${(await Threads.getInfo(threadID)).threadName || "unknown"}`);
+            api.sendMessage(text, handleReply.threadID, (err, info) => {
+                atmDir.forEach(each => fs.unlinkSync(each))
+                atmDir = [];
+                global.client.handleReply.push({
+                    name: this.config.name,
+                    type: "reply",
+                    messageID: info.messageID,
+                    messID: messageID,
+                    threadID
+                })
+            });
+            break;
+        }
+        case "reply": {
+            let text = `admin ${name} replied to you\n\nreply : ${body}\n\nreply to this message if you want to respond again.`;
+            if(event.attachments.length > 0) text = await getAtm(event.attachments, `${body}${name} replied to you\n\nreply to this message if you want to respond again.`);
+            api.sendMessage(text, handleReply.threadID, (err, info) => {
+                atmDir.forEach(each => fs.unlinkSync(each))
+                atmDir = [];
+                global.client.handleReply.push({
+                    name: this.config.name,
+                    type: "sendnoti",
+                    messageID: info.messageID,
+                    threadID
+                })
+            }, handleReply.messID);
+            break;
+        }
+    }
+}
+
+module.exports.run = async function ({ api, event, args, Users }) {
+    const moment = require("moment-timezone");
+      var gio = moment.tz("Asia/Manila").format("DD/MM/YYYY - HH:mm:s");
+    const { threadID, messageID, senderID, messageReply } = event;
+    if (!args[0]) return api.sendMessage("please input message", threadID);
+    let allThread = global.data.allThreadID || [];
+    let can = 0, canNot = 0;
+    let text = `message from admins\n\ntime : ${gio}\nadmin name : ${await Users.getNameUser(senderID)}\nmessage : ${args.join(" ")}\n\nreply to this message if you want to respond from this announce.`;
+    if(event.type == "message_reply") text = await getAtm(messageReply.attachments, `message from admins\n\ntime : ${gio}\nadmin name : ${await Users.getNameUser(senderID)}\nmessage : ${args.join(" ")}\n\nreply to this message if you want to respond from this announce.`);
+    await new Promise(resolve => {
+        allThread.forEach((each) => {
+            try {
+                api.sendMessage(text, each, (err, info) => {
+                    if(err) { canNot++; }
+                    else {
+                        can++;
+                        atmDir.forEach(each => fs.unlinkSync(each))
+                        atmDir = [];
+                        global.client.handleReply.push({
+                            name: this.config.name,
+                            type: "sendnoti",
+                            messageID: info.messageID,
+                            messID: messageID,
+                            threadID
+                        })
+                        resolve();
+                    }
+                })
+            } catch(e) { console.log(e) }
+        })
+    })
+    api.sendMessage(`send to ${can} thread, not send to ${canNot} thread`, threadID);
+}
---
+++ info.js
@@ -0,0 +1,47 @@
+module.exports.config = {
+    name: "admin",
+    version: "1.0.0",
+    permission: 0,
+    credits: "nayan",
+    prefix: true,
+    description: "",
+    category: "prefix",
+    usages: "",
+    cooldowns: 5,
+    dependencies: 
+	{
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+const time = process.uptime(),
+		hours = Math.floor(time / (60 * 60)),
+		minutes = Math.floor((time % (60 * 60)) / 60),
+		seconds = Math.floor(time % 60);
+const moment = require("moment-timezone");
+var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
+  
+var callback = () => api.sendMessage({body:`
+--------------------------------------------
+𝐍𝐚𝐦𝐞       : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 Ashik 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 Ashik 
+𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: Sirajganj , Rajshahi 
+𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: Sirajganj , 𝐃𝐡𝐚ka 𝐁𝐲𝐩𝐚𝐬𝐬
+𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
+𝐀𝐠𝐞           : 𝟏8+
+𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
+𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
+𝐆𝐦𝐚𝐢𝐥       : lovelover2420@gmail.com
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801310543404
+𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/‌
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/profile.php?id=100069227486157`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+    fs.unlinkSync(__dirname + "/cache/1.png"));  
+      return request(encodeURI(`https://graph.facebook.com/100069227486157/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
+   };
